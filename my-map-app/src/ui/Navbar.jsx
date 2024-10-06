61bc0f5aa29aaa8c8fb119b71cdefdb1edff64ed
Jerry <jerryshum2636@gmail.com>
2024-10-05 22:12:44 -0400

    make navbar

--- my-map-app/src/ui/Navbar.jsx
+++ my-map-app/src/ui/Navbar.jsx
@@ -1,5 +1,28 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-  return <div className="w-full bg-red-500"> THis is the navbar</div>;
+  return (
+    <nav className="bg-vistablue flex h-full w-full items-center justify-between px-10 py-2">
+      <ul className="flex gap-10">
+        <li>
+          <Link to={"/"} className="text-2xl font-light uppercase">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to={"/map"} className="text-2xl font-light uppercase">
+            Map
+          </Link>
+        </li>
+        <li>
+          <Link to={"/about"} className="text-2xl font-light uppercase">
+            About Us
+          </Link>
+        </li>
+      </ul>
+      <img src="/mapxlogo.png" alt="mappingxlogo" className="h-14" />
+    </nav>
+  );
 }
 
 export default Navbar;
