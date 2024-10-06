a61d6ee81eae6185bcb4baa9c25829ff11b4c468
Jerry <jerryshum2636@gmail.com>
2024-10-05 21:40:10 -0400

    tailwind color themes

--- my-map-app/tailwind.config.js
+++ my-map-app/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        choco: "#4A051c",
+        vistablue: "#7EA3cc",
+        copper: "#a8763e",
+        indigodye: "#0c4767",
+        lightblue: "#90c2e7",
+      },
+    },
   },
   plugins: [],
 };
