f725e563f009be491dac8eed1863057a9ed0e41a
Jerry <jerryshum2636@gmail.com>
2024-10-06 00:06:57 -0400

    made map1 page - still need to add vectors and bg image

--- my-map-app/src/pages/Map.jsx
+++ my-map-app/src/pages/Map.jsx
@@ -1,5 +1,20 @@
 function Map() {
-  return <div></div>;
+  return (
+    <div className="bg-cream grid grid-cols-2 gap-32 px-48">
+      <div>
+        <h1 className="mt-24 text-center text-6xl font-semibold">Map</h1>
+        <div className="bg-lightblue mt-10 h-3/5 rounded-[80px] p-10">
+          description goes here
+        </div>
+      </div>
+      <div className="mt-24 flex flex-col items-center gap-10">
+        <div className="h-3/5 w-full bg-gray-300 p-20 text-center">map</div>
+        <button className="w-2/4 rounded-lg bg-orange-300 p-5 text-2xl font-semibold uppercase">
+          Go yessir
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Map;
