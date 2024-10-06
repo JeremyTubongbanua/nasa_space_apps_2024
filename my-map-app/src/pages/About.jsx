f81990a14af9fad43fde1fd1e84131b712feb639
Jerry <jerryshum2636@gmail.com>
2024-10-05 23:29:33 -0400

    about page

--- my-map-app/src/pages/About.jsx
+++ my-map-app/src/pages/About.jsx
@@ -1,5 +1,24 @@
 function About() {
-  return <div className="bg-blue-400">This is thea bout page</div>;
+  return (
+    <div className="bg-cream bg-aboutbg grid grid-cols-2 bg-contain bg-no-repeat px-48">
+      <img
+        src="/public/Earth Vector.svg"
+        alt=""
+        className="scale-125 place-self-center"
+      />
+      <img
+        src="/public/Satellite Vector.png"
+        alt=""
+        className="absolute bottom-10 left-10 rotate-90 scale-150"
+      />
+      <div>
+        <h1 className="mt-24 text-center text-6xl font-semibold">About Us</h1>
+        <div className="bg-lightblue mt-10 h-3/5 rounded-3xl p-10">
+          description goes here
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
