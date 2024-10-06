04d75a2d808ddaa76418aee9a3234ec0b05ad2c9
Jerry <jerryshum2636@gmail.com>
2024-10-06 15:09:22 -0400

    Aboutpage fix

--- my-map-app/src/pages/About.jsx
+++ my-map-app/src/pages/About.jsx
@@ -54,17 +54,28 @@ function About() {
 
       <div>
         <h1 className="mt-24 text-center text-6xl font-semibold">About Us</h1>
-        <div className="mt-10 h-3/5 rounded-3xl bg-lightblue p-10">
+        <div className="mt-10 flex flex-col gap-4 rounded-3xl bg-lightblue p-10">
           <p>
             Mapping X was created for the "Community Mapping" Challenge for the
             NASA Space Apps 2024 Oshawa Event. Authors of this project are
-            Jeremy Mark Tubongbanua, Jeska Maya Tubongbanua, and Jerry Shum
+            Jeremy Mark Tubongbanua, Jeska Maya Tubongbanua, and Jerry Shum.
           </p>
-          Figma Prototyping:
-          <img src="https://i.imgur.com/xLHJ1nM.png" alt="figma prototyping" />
-
-        How It Works:
-          <img src="https://i.imgur.com/EGqX8QF.png" alt="how it works" />
+          <div>
+            Figma Prototyping:
+            <img
+              src="https://i.imgur.com/xLHJ1nM.png"
+              alt="figma prototyping"
+              className="rounded-lg"
+            />
+          </div>
+          <div>
+            How It Works:
+            <img
+              src="https://i.imgur.com/EGqX8QF.png"
+              alt="how it works"
+              className="rounded-lg"
+            />
+          </div>
         </div>
       </div>
     </div>
